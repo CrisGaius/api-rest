2411d8f1ee4edeb367623e3d98eb81808ba83a55
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-10-06 21:32:49 -0300

    Initial Commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+export default [
+  { languageOptions: { globals: globals.node } },
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "semi": ["error", "always"], // Sempre exigir ponto e vírgula
+      "quotes": ["error", "double"], // Exigir aspas duplas
+      "no-console": "warn", // Avisar quando usar `console.log`
+      "indent": ["error", 2], // Exigir indentação de 2 espaços
+    }
+  }
+];
