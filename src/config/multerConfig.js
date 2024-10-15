8e4e18c7d17d266167bdfb51009a8a203e68c1de
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-10-14 21:02:54 -0300

    Fazendo o tratamento do tipo do arquivo enviado.

--- src/config/multerConfig.js
+++ src/config/multerConfig.js
@@ -4,6 +4,11 @@ import { extname, resolve } from "path";
 const random = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
+  fileFilter: (request, file, cb) => {
+    if (file.mimetype !== "image/png" && file.mimetype !== "image/jpeg" && file.mimetype !== "image/jpg") return cb(new multer.MulterError("File must be an image. Formats accepted: png, jpg, jpeg."));
+
+    return cb(null, true);
+  },
   storage: multer.diskStorage({
     destination: (request, file, cb) => {
       cb(null, resolve(__dirname, "..", "..", "uploads"));
