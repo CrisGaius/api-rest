269cc347522bf6d87a0d9c0195037a218bdb135a
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-10-23 22:25:07 -0300

    fixing CORS problem

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,8 @@ _dotenv2.default.config();
 require('./database/connection');
 
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 var _path = require('path');
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
@@ -11,6 +13,21 @@ var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopReq
 var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interopRequireDefault(_tokenRoutes);
 var _studentRoutes = require('./routes/studentRoutes'); var _studentRoutes2 = _interopRequireDefault(_studentRoutes);
 var _studentPhotoRoutes = require('./routes/studentPhotoRoutes'); var _studentPhotoRoutes2 = _interopRequireDefault(_studentPhotoRoutes);
+
+const whiteList = [
+  "http://34.95.206.56:81",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  origin: function (origin, cb) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
 class Application {
   constructor() {
@@ -20,6 +37,8 @@ class Application {
   }
 
   middlewares() {
+    this.application.use(_cors2.default.call(void 0, corsOptions));
+    this.application.use(_helmet2.default.call(void 0, ));
     this.application.use(_express2.default.urlencoded({ extended: true }));
     this.application.use(_express2.default.json());
     this.application.use("/images", _express2.default.static(_path.resolve.call(void 0, __dirname, "..", "uploads", "images")));
--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ dotenv.config();
 import "./database/connection";
 
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 import { resolve } from "path";
 
 import homeRoutes from "./routes/homeRoutes";
@@ -11,6 +13,21 @@ import userRoutes from "./routes/userRoutes";
 import tokenRoutes from "./routes/tokenRoutes";
 import studentRoutes from "./routes/studentRoutes";
 import studentPhotoRoutes from "./routes/studentPhotoRoutes";
+
+const whiteList = [
+  "http://34.95.206.56:81",
+  "http://localhost:3000",
+];
+
+const corsOptions = {
+  origin: function (origin, cb) {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error("Not allowed by CORS"));
+    }
+  }
+};
 
 class Application {
   constructor() {
@@ -20,6 +37,8 @@ class Application {
   }
 
   middlewares() {
+    this.application.use(cors(corsOptions));
+    this.application.use(helmet());
     this.application.use(express.urlencoded({ extended: true }));
     this.application.use(express.json());
     this.application.use("/images", express.static(resolve(__dirname, "..", "uploads", "images")));
