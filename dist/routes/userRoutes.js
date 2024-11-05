940594857c670ecfb57f84a3c85eb4a0a08d679e
Cristiano Gomes <101535034+CrisGaius@users.noreply.github.com>
2024-11-04 22:18:08 -0300

    Update dist for production

--- dist/routes/userRoutes.js
+++ dist/routes/userRoutes.js
@@ -8,7 +8,7 @@ const router = new (0, _express.Router)();
 //router.get("/", loginRequired, userController.index); // Lista usuários
 //router.get("/:id", userController.show); // Lista usuário
 
-router.post("/", _loginRequired2.default, _UserController2.default.storeUser);
+router.post("/", _UserController2.default.storeUser);
 
 router.put("/", _loginRequired2.default, _UserController2.default.update);
 
